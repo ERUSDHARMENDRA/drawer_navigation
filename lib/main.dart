a6e17f00776673ab321e08833ff397487159c3b3
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-03-11 11:53:42 +0545

    "Version 2"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:drawer_navigation/newPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -7,14 +8,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
     debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+        primaryColor: defaultTargetPlatform == TargetPlatform.iOS ? Colors.white :null),
       home: HomePage(),
+      // routes: <String, WidgetBuilder>{
+      // "/a" :(BuildContext context) => NewPage("NewPage"),}
     );
   }
 }
@@ -22,11 +26,13 @@ class MyApp extends StatelessWidget {
 
 //HomePage Stateful Widget
 class HomePage extends StatefulWidget {
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,17 +46,39 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             UserAccountsDrawerHeader(
                 accountName: Text("Erus008"),
-                accountEmail: Text("Dharmendramehta622@gmail.com")
+                accountEmail: Text("Dharmendramehta622@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Theme.of(context).platform == TargetPlatform.iOS ? Colors.deepPurple:Colors.white,
+                child: Text("E"),
+              ),
+              otherAccountsPictures:<Widget> [
+                CircleAvatar(
+                  backgroundColor: Theme.of(context).platform == TargetPlatform.iOS ? Colors.deepPurple:Colors.white,
+                  child: Text("K"),
+                ),
+              ],
             ),
 
             ListTile(
               title: Text("Page One"),
               trailing: Icon(Icons.arrow_upward_rounded),
+              onTap: ()  {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        NewPage("Page One")));
+              },
             ),
 
             ListTile(
               title: Text("Page Two"),
               trailing: Icon(Icons.arrow_downward_rounded),
+              onTap: ()  {
+                Navigator.of(context).pop();
+                 Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                   NewPage("Page Two")));
+                },
             ),
 
             Divider(),
@@ -58,6 +86,7 @@ class _HomePageState extends State<HomePage> {
             ListTile(
               title: Text("Close"),
               trailing: Icon(Icons.close),
+              onTap: ()=> Navigator.of(context).pop(),
             ),
 
           ],
